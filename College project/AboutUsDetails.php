43a9c49160c8bbb9bd4efe517f8cd9bf8ea0e70b
FlexiDriod <bcaknowledgeplatform@gmail.com>
2024-06-22 03:25:25 +0530

    Add files via upload

---
+++ College project/AboutUsDetails.php
@@ -0,0 +1,71 @@
+<?php
+function aboutus_show(){
+echo "
+<main role='main'>
+<style>
+        .gradient-bg {
+            background-color: #f5df2e;
+background-image: linear-gradient(315deg, #f5df2e 0%, #f07654 74%);
+        }
+    </style>
+    <div class='album py-5 gradient-bg'>
+        <div class='container' style='padding-top: 50px;'>
+            <div class='row'>
+                <div class='col-md-4'>
+                    <div class='card mb-4 box-shadow'>
+                        <img class='card-img-top'
+                            data-src='holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail'
+                            alt='Thumbnail [100%x225]' style='height: 225px; width: 100%; display: block;'
+                            data-holder-rendered='true' src='M5320856.jpg'>
+                        <div class='card-body'>
+                            <p class='card-text text-center'><b>Every Drop a Beacon of Hope: Power the Fight Against Thalassemia</b></p><br>
+                            <p class='text-center'>Thalassemia, a condition critical for life, underscores the vital need for blood. Our website simplifies the search for compatible donors with just a click. Let's join hands to ensure those battling thalassemia never stand alone.</p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class='col-md-4'>
+                    <div class='card mb-4 box-shadow'>
+                        <img class='card-img-top'
+                            data-src='holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail'
+                            alt='Thumbnail [100%x225]' style='height: 225px; width: 100%; display: block;'
+                            data-holder-rendered='true' src='hematologyclinichero.jpg'>
+                        <div class='card-body '>
+                            <p class='card-text text-center'><b>Unlock the Power of Every Drop: Unveil Hemophilia Heroes on Our Website!</b></p>
+                            <br>
+                            <p class='text-center'>
+                            Hemophilia is a challenging journey, where time is crucial for the right blood. Finding donors can be like searching for a needle in a haystack. With Blood Collective Alliance, we connect patients and donors swiftly and efficiently.
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+                <div class='col-md-4'>
+                    <div class='card mb-4 box-shadow'>
+                        <img class='card-img-top'
+                            data-src='holder.js/100px225?theme=thumb&amp;bg=55595c&amp;fg=eceeef&amp;text=Thumbnail'
+                            alt='Thumbnail [100%x225]' style='height: 225px; width: 100%; display: block;'
+                            data-holder-rendered='true' src='Untitled.jpeg'>
+                        <div class='card-body'>
+                            <p class='card-text text-center'>
+                            <b>Every Drop a Beacon of Hope: Donate Blood, Power the Fight Against Leukemia</b>
+                            </p><br>
+                            <p class='text-center'>
+                            Leukemia, a blood cancer, steals your body's defense warriors (white blood cells). <br> Fight back! Find compatible donors instantly with . It’s a vision that fuels all those who support Leukemia patient and healthcare advocacy.
+                        </p>
+                            <div class='d-flex justify-content-between align-items-center'>
+                                <div class='btn-group'>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</main>
+";
+}
+aboutus_show();
+?>
---
+++ College project/Contributors.php
@@ -0,0 +1,50 @@
+
+
+<?php
+function display_contributers(){
+echo"
+<div class='bg-dark col pb-5 pt-5 px-md-5'>
+        <div><h1 style='color:white;font-size 20vw; ' class='text-center fw-bolder '><b>Contribution</b>
+            </h1></div>
+            
+            <div>
+    <div class='container justify-align-items-center'>
+      <ul class='team me-5'>
+        <li class='member <!--co-funder-->'>
+          <div class='thumb'>
+            <img src='Sudip.jpg'>
+          </div>
+          <!--X:\IMG_16219.jpg-->
+          <div class='description'>
+
+            <p><strong>
+              Sudip, a final-year BCA student at Burdwan Raj College, took the lead in creating the 'Blood Collective Alliance' website with his teammate. His expertise lies in Java, Dart, PHP, PostgreSQL, and Flutter. Together with Trinankur, he managed both the frontend design and backend database using HTML, CSS, Bootstrap, PHP and PostgreSQL.<br>
+              Beyond academia, Sudip worked as a Hybrid App Developer at Elphill Technology Pvt. Ltd., where he focused on his passion for mobile app development.
+              Sudip's dedication to bridging technology with practical solutions underscores his commitment to innovation and excellence in web and mobile app development.
+              <br><a href='https://www.linkedin.com/in/sudip-howlader-7638532a9'>@Sudip Howlader</a>
+            </strong></p>
+          </div>
+        </li>
+
+        <li class='member'>
+          <div class='thumb'>
+            <img src='trinankur.png'>
+          </div>
+          <div class='description'>
+            
+            <p><b>
+              Trinankur, a final-year BCA student at Burdwan Raj College, played a crucial role in bringing the 'Blood Collective Alliance' website to life. His expertise spans C/C++, C#, .NET, Python, and PHP. In collaboration with Sudip Howlader, Trinankur took charge of backend development using PHP, providing essential guidance and technical leadership throughout the project.
+              Trinankur was instrumental in transforming Sudip's idea into a tangible reality, leveraging his skills to ensure the project's success. His proficiency in backend technologies and collaborative spirit were pivotal in the development and implementation of the website.
+              <br>
+              <a href='https://www.linkedin.com/in/trinankur-samanta-3ab687150'>@Trinankur Samanta</a>
+            </b></p>
+          </div>
+        </li>
+      </ul>
+    </div>
+</div>
+</div>
+";
+}
+display_contributers();
+?>
